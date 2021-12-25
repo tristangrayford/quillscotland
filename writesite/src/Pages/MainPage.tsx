d27257dc1b6383f79c049fa8f679689ddf8e76c5
Tristan Gray <tristangraycc@gmail.com>
2021-12-25 17:39:05 +0000

    Startup updates

---
+++ writesite/src/Pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class MainPage extends React.Component {
+    public constructor(props: {}) {
+        super(props);
+    }
+
+    public render(): JSX.Element {
+        return <h1>Tristan Gray</h1>
+    }
+}
