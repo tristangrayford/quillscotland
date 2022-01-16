39b9bd408c12e55762db46b4bd71b178fef4a716
Tristan Gray <tristangraycc@gmail.com>
2022-01-16 03:26:49 +0000

    Undo pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,26 @@ import ReactDOM from 'react-dom';
 import MainPage from './Pages/Main/MainPage';
 import reportWebVitals from './reportWebVitals';
 import './App.scss'
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { HashRouter , Routes, Route } from 'react-router-dom';
 import AboutPage from './Pages/About/AboutPage';
 import LanguagePage from './Pages/Language/LanguagePage';
 import RecommendationsPage from './Pages/Recommendations/RecommendationsPage';
 import OtherWritingPage from './Pages/OtherWriting/OtherWritingPage';
 
 const routing = (
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage/>} />
         <Route path="/About" element={<AboutPage/>} />
         <Route path="/Languages" element={<LanguagePage/>} />
         <Route path="/Recommendations" element={<RecommendationsPage/>} />
         <Route path="/Writing" element={<OtherWritingPage/>} />
-      </Routes>
+        </Routes>
+    </HashRouter>
 )
 
-ReactDOM.render(<BrowserRouter  basename={process.env.PUBLIC_URL}>{routing}</BrowserRouter >, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
