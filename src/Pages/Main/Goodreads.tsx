0a5b82fa60cd5a3fd54d8ed77048d3be0674c854
Tristan Gray <tristangraycc@gmail.com>
2023-11-13 21:46:08 +0000

    Adding goodreads reviews

--- src/Pages/Main/Goodreads.tsx
+++ src/Pages/Main/Goodreads.tsx
@@ -39,45 +39,51 @@ function GoodreadsWidget() {
                         </div>
                     </div>
 
-                <div className="gr_book_container">
-                    <a title="A Gift of the Sea (Tales of Seann Àite #3)" className="gr_book_image" rel="nofollow" href="https://www.goodreads.com/book/show/58399751-a-gift-of-the-sea?utm_medium=api&utm_source=author_widget">
-                        <img alt="A Gift of the Sea" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1624380459l/58399751._SX50_.jpg" />
-                    </a>
-                    <a className="gr_book_title" rel="nofollow" href="https://www.goodreads.com/book/show/58399751-a-gift-of-the-sea?utm_medium=api&utm_source=author_widget">
-                        A Gift of the Sea (#3)
-                    </a>
-                    <br />
-                    <div className="gr_review_stats">
-                        reviews: 18
+                    <div className="gr_book_container">
+                        <a title="A Gift of the Sea (Tales of Seann Àite #3)" className="gr_book_image" rel="nofollow" href="https://www.goodreads.com/book/show/58399751-a-gift-of-the-sea?    utm_medium=api&utm_source=author_widget">
+                            <img alt="A Gift of the Sea" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1624380459l/58399751._SX50_.jpg" />
+                        </a>
+                        <a className="gr_book_title" rel="nofollow" href="https://www.goodreads.com/book/show/58399751-a-gift-of-the-sea?utm_medium=api&utm_source=author_widget">
+                            A Gift of the Sea (#3)
+                        </a>
                         <br />
-                        ratings: 22 <br />(avg rating 4.68)
+                        <div className="gr_review_stats">
+                            reviews: 18
+                            <br />
+                            ratings: 22 <br />(avg rating 4.68)
+                        </div>
                     </div>
-                </div>
 
-                <div className="gr_book_container">
-                    <a title="The Woven Thread (Tales of Seann Àite #4)" className="gr_book_image" rel="nofollow" href="https://www.goodreads.com/book/show/60679169-the-woven-thread?utm_medium=api&utm_source=author_widget"><img alt="The Woven Thread" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1648828392l/60679169._SX50_.jpg" /></a>
-                    <a className="gr_book_title" rel="nofollow" href="https://www.goodreads.com/book/show/60679169-the-woven-thread?utm_medium=api&utm_source=author_widget">The Woven Thread (#4)</a>
-                    <br />
-                    <div className="gr_review_stats">
-                        reviews: 9
+                    <div className="gr_book_container">
+                        <a title="The Woven Thread (Tales of Seann Àite #4)" className="gr_book_image" rel="nofollow" href="https://www.goodreads.com/book/show/60679169-the-woven-thread?  utm_medium=api&utm_source=author_widget"><img alt="The Woven Thread" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1648828392l/60679169. _SX50_.jpg" /></a>
+                        <a className="gr_book_title" rel="nofollow" href="https://www.goodreads.com/book/show/60679169-the-woven-thread?utm_medium=api&utm_source=author_widget">The Woven  Thread (#4)</a>
                         <br />
-                        ratings: 13 <br />(avg rating 4.69)
+                        <div className="gr_review_stats">
+                            reviews: 9
+                            <br />
+                            ratings: 13 <br />(avg rating 4.69)
+                        </div>
                     </div>
-                </div>
 
-                <div className="gr_book_container">
-                    <a title="Eyes of Gold (Tales of Seann Àite #5)" className="gr_book_image" rel="nofollow" href="https://www.goodreads.com/book/show/195855750-eyes-of-gold?utm_medium=api&utm_source=author_widget">
-                        <img alt="Eyes of Gold" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1695614990l/195855750._SX50_.jpg" />
-                    </a>
-                    <a className="gr_book_title" rel="nofollow" href="https://www.goodreads.com/book/show/195855750-eyes-of-gold?utm_medium=api&utm_source=author_widget">Eyes of Gold (#5)</a>
-                    <br />
-                    <div className="gr_review_stats">
-                        reviews: 2
+                    <div className="gr_book_container">
+                        <a title="Eyes of Gold (Tales of Seann Àite #5)" className="gr_book_image" rel="nofollow" href="https://www.goodreads.com/book/show/195855750-eyes-of-gold? utm_medium=api&utm_source=author_widget">
+                            <img alt="Eyes of Gold" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1695614990l/195855750._SX50_.jpg" />
+                        </a>
+                        <a className="gr_book_title" rel="nofollow" href="https://www.goodreads.com/book/show/195855750-eyes-of-gold?utm_medium=api&utm_source=author_widget">Eyes of Gold (#5)</a>
                         <br />
-                        ratings: 4 <br />(avg rating 4.75)
+                        <div className="gr_review_stats">
+                            reviews: 2
+                            <br />
+                            ratings: 4 <br />(avg rating 4.75)
+                        </div>
                     </div>
                 </div>
-                
+            </div>
+                <div id="goodreads-widget">
+                    <div id="gr_header"><h1><a rel="nofollow" href="https://www.goodreads.com/book/show/55061225-whispers-to-a-crow">Goodreads reviews for Whispers to a Crow</a></h1></div>
+                        <iframe title="the_iframe" src="https://www.goodreads.com/api/reviews_widget_iframe?did=DEVELOPER_ID&format=html&header_text=Goodreads+reviews+for+Whispers+to+a+Crow&isbn=1838485406&links=660&review_back=fff&stars=000&text=000" width="565" height="400"></iframe>
+                    <div id="gr_footer">
+                    <a className="gr_branding" target="_blank" rel="nofollow noopener noreferrer" href="https://www.goodreads.com/book/show/55061225-whispers-to-a-crow?utm_medium=api&utm_source=reviews_widget">Reviews from Goodreads.com</a>
                 </div>
             </div>
         </div>
